69a0211b891dad063bae1b8b188f974da133a636
ThiagoLuzFerreira <tlrferreira83@gmail.com>
2024-02-19 08:23:48 -0300

    config for config server

---
+++ src/test/java/com/thiago/demoprojectconfigserver/DemoProjectConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.demoprojectconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
